af2d992b911d89c7a5efaf89f959b92adc06aa26
MxhZjl <2650315809@qq.com>
2025-12-26 23:39:46 +0800

    测试

---
+++ web-ai-project01/maven-project01/src/test/java/com/test/UserServiceTest.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    @Test
+    public void testGetAge() {
+        UserService userService=new UserService();
+    }
+}
