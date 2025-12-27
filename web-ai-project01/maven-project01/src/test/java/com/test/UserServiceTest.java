b1c01a13d25e6ca337131744f55d5df0d0710572
MxhZjl <2650315809@qq.com>
2025-12-27 22:40:08 +0800

    ceshi

--- web-ai-project01/maven-project01/src/test/java/com/test/UserServiceTest.java
+++ web-ai-project01/maven-project01/src/test/java/com/test/UserServiceTest.java
@@ -6,5 +6,17 @@ public class UserServiceTest {
     @Test
     public void testGetAge() {
         UserService userService=new UserService();
+        Integer age=userService.getAge("100000200010011011");
+        System.out.println(age);
+    }
+    @Test
+    public void testGetGender() {
+        UserService userService=new UserService();
+        String gender = userService.getGender("100000200010011011");
+        System.out.println();
     }
 }
+/*
+* 基于断言
+*/
+
